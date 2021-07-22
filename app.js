dac32cf7791e493b419cee7f9884494d9f86ec1f
Kshitij Kumar <kshitij1999@gmail.com>
2021-07-22 20:30:42 +0530

    created a weather app

---
+++ app.js
@@ -0,0 +1,62 @@
+const iconElement = document.querySelector('.weather-icon');
+const tempElement = document.querySelector('.temperature-value p');
+const descElement = document.querySelector('.temperature-description');
+const locationElement = document.querySelector('.location p');
+const notificationElement = document.querySelector('.notification');
+
+const weather = {};
+weather.temperature = {
+    unit: 'celsius'
+};
+
+const KELVIN = 273;
+const key = '96f6ba150dbfde1782395bffef95d6a2';
+
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else{
+    notificationElement.style.display = 'block';
+    notificationElement.innerHTML = "<p>Browser doesn't support Geolocalization";
+}
+
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+
+function showError(error){
+    notificationElement.style.display = 'block';
+    notificationElement.innerHTML = `<p>${error.message}`;
+}
+
+//Get weather from API provider
+function getWeather(latitude, longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    console.log(api);
+
+    fetch(api)
+    .then(function(response){
+        let data = response.json();
+        return data;
+    })
+    .then(function(data){
+        weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+        weather.description = data.weather[0].description;
+        weather.iconId = data.weather[0].icon;
+        weather.city = data.name;
+        weather.country = data.sys.country;
+    })
+    .then(function(){
+        displayWeather();
+    });
+}
+
+//Display weather
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}° <span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
